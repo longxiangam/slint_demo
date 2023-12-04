a1ee2e927452cf49ce5b503d389b7b03351fbdf3
lx-air <longxiangam@163.com>
2023-12-04 22:36:09 +0800

    在 st7735 中能成功显示

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@
 extern crate alloc;
 
 use alloc::rc::Rc;
+use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
+use embedded_graphics::mono_font::ascii::FONT_6X9;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::primitives::{PrimitiveStyle, Triangle};
+use embedded_graphics::text::Text;
+use embedded_graphics_core::draw_target::DrawTarget;
+use embedded_graphics_core::Drawable;
+use embedded_graphics_core::geometry::Point;
+use embedded_graphics_core::pixelcolor::Rgb565;
+use embedded_graphics_core::prelude::RgbColor;
 use fugit::HertzU32;
 #[cfg(not(feature = "simulator"))]
 use hal::entry;
@@ -18,6 +28,7 @@ use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::Time
 use fugit::RateExtU32;
 use slint::platform::WindowEvent;
 use hal::gpio::Unknown;
+use st7735_lcd::Orientation;
 
 fn create_slint_app() -> AppWindow {
     let ui = AppWindow::new().expect("Failed to load UI");
@@ -97,6 +108,31 @@ fn main() -> !{
     let height = 160;
 
     let mut display = st7735_lcd::ST7735::new(spi, dc, rst, rgb, inverted, width, height);
+    display.init(&mut delay).unwrap();
+    display.clear( Rgb565::BLACK).unwrap();
+    display
+        .set_orientation(&Orientation::Portrait)
+        .unwrap();
+    display.set_offset(0, 0);
+    let yoffset = 100;
+    let image_raw: ImageRawLE<Rgb565> =
+        ImageRaw::new(include_bytes!("../assets/ferris.raw"), 86);
+    let image = Image::new(&image_raw, Point::new(26, 8));
+    image.draw(&mut display).unwrap();
+  /*  let thin_stroke = PrimitiveStyle::with_stroke(Rgb565::WHITE, 1);
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+        .into_styled(thin_stroke)
+        .draw(&mut display).expect("绘制失败");*/
+
+    let character_style = MonoTextStyle::new(&FONT_6X9, Rgb565::WHITE);
+
+    let text = Text::new("Hello e-g", Point::new(10, 11), character_style);
+    text.draw(&mut display).expect("");
+
 
     let window = slint::platform::software_renderer::MinimalSoftwareWindow::new(Default::default());
     slint::platform::set_platform(alloc::boxed::Box::new(MyPlatform {
